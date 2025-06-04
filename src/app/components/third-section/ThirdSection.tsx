363064783b025ea69535c20bbf987227d4961b6b
Michael <michaelrichard7020@gmail.com>
2025-06-04 23:31:56 +0530

    fixed padding p in hero

---
+++ src/app/components/third-section/ThirdSection.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './ThirdSection.css'
+
+
+function ThirdSection() {
+  return (
+    <div>
+        <h3 className='y-kingdom-catering-q-h3'>Why Choose Kingdom Catering?</h3>
+    </div>
+  )
+}
+
+export default ThirdSection
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "./components/hero/hero";
 import "./global.css";
 import SecondSection from "./components/second-section/SecondSection";
+import ThirdSection from "./components/third-section/ThirdSection";
 
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
     <div className="components-container">
       <Hero />
       <SecondSection />
+      <ThirdSection />
     </div>
     
   );
